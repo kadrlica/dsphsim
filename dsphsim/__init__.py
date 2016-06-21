cbdc21b9129635e7e77510a5da3200016a95e43d
Alex Drlica-Wagner <kadrlica@fnal.gov>
2016-06-21 15:27:04 -0500

    Adding some author info

--- dsphsim/__init__.py
+++ dsphsim/__init__.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+__author__ = "Alex Drlica-Wagner"
+__email__ = "kadrlica@fnal.gov"
+
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
