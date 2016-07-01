77f97d4a442dbad47de4b32a0c442f33f7fe04f4
Alex Drlica-Wagner <kadrlica@fnal.gov>
2016-06-30 20:55:50 -0400

    Adding install option

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     scripts = ['bin/dsphsim'],
     install_requires=[
         'python >= 2.7.0',
-        'ugali >= 1.6.0 --install-option "--isochrones"',
+        'ugali >= 1.6.0',
         'vegas >= 3.0',
     ],
     packages=['dsphsim'],
